9ac2d5e1473e68fc090cc57ac7e2ff661f884b95
Atul Mehta <atulmehta0510@gmail.com>
2023-12-26 00:08:00 +0530

    Added new files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'slambot_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='atul22',
+    maintainer_email='atul22@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "slambot_keyboard_controller = slambot_controller.slambot_keyboard_controller:main"
+        ],
+    },
+)
---
+++ slambot_controller/slambot_keyboard_controller.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import serial                                   # Import the serial library
+
+class KeyboardSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('slambot_keyboard_controller')
+        self.subscription = self.create_subscription(Twist, '/cmd_vel', self.twist_callback, 3)
+
+        # Serial communication configuration
+        self.port = '/dev/ttyUSB0'  # Replace with your actual serial port
+        self.baudrate = 115200  # Adjust to match your device's baud rate
+
+        try:
+            self.ser = serial.Serial(self.port, self.baudrate)
+            print("Serial port opened successfully.")
+        except serial.SerialException as e:
+            print("Error opening serial port:", e)
+            exit()
+
+    def twist_callback(self, msg):
+        linear_x = msg.linear.x
+        angular_z = msg.angular.z
+
+        print("Received keyboard input:")
+        print(f"- Linear X: {linear_x:.2f}")
+        print(f"- Angular Z: {angular_z:.2f}")
+
+        # Format data as a string and send over serial
+        data_to_send = f"Linear_X: {linear_x:.2f} ||Angular_Z: {angular_z:.2f} \n".encode()  # Add newline for clarity
+        self.ser.write(data_to_send)
+        print(f"Data sent over serial: Linear X: {linear_x:.2f},Angular Z: {angular_z:.2f}\n")
+
+def main(args=None):
+    rclpy.init(args=args)
+    keyboard_subscriber = KeyboardSubscriber()
+    rclpy.spin(keyboard_subscriber)
+    keyboard_subscriber.destroy_node()
+    keyboard_subscriber.ser.close()  # Close the serial port
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
