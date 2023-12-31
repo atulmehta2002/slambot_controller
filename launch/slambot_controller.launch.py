0c04e5b6b3ee1713a29237611d648ac9122391da
atulmehta2002 <atulmehta0510@gmail.com>
2024-01-01 00:58:58 +0530

    removed teleop node from launch

--- launch/slambot_controller.launch.py
+++ launch/slambot_controller.launch.py
@@ -5,12 +5,10 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         launch_ros.actions.Node(
-                package='teleop_twist_keyboard',
-                executable='teleop_twist_keyboard',
-            ),
-        launch_ros.actions.Node(
                 package='slambot_controller',
                 executable='slambot_keyboard_controller',
+                output="screen",
+                emulate_tty=True
             ),
     ])
 
